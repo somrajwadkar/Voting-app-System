317e076ea478d860767246d19fb8112dec6a5ab5
somraj wadkar <somrajwadkar2@gmail.com>
2026-05-31 21:07:16 +0530

    the latest commit

--- models/routes/candidateRoute.java
+++ models/routes/candidateRoute.java
@@ -7,7 +7,14 @@ const User = require('../models/user');
 
 
 const checkAdminRole = async (userID) => {
-   
+   try{
+        const user = await User.findById(userID);
+        if(user.role === 'admin'){
+            return true;
+        }
+   }catch(err){
+        return false;
+   }
 }
 
 router.post('/', jwtAuthMiddleware, async (req, res) =>{

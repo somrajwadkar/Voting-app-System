8dfe4d1a44e5cb66f1f110d0291baf168901478d
somraj wadkar <somrajwadkar2@gmail.com>
2026-06-01 22:39:12 +0530

    the latest commmit here

--- models/user.js
+++ models/user.js
@@ -62,15 +62,7 @@ userSchema.pre('save', async function(next){
     }
 })
 
-userSchema.methods.comparePassword = async function(candidatePassword){
-    try{
-        // Use bcrypt to compare the provided password with the hashed password
-        const isMatch = await bcrypt.compare(candidatePassword, this.password);
-        return isMatch;
-    }catch(err){
-        throw err;
-    }
-}
+
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;

9ca48dfc4aa245d0086de71591752546cce99740
somraj wadkar <somrajwadkar2@gmail.com>
2026-06-01 20:56:25 +0530

    the latest commmit here

--- models/routes/candidateRoute.java
+++ models/routes/candidateRoute.java
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
-
-
-
+const {jwtAuthMiddleware, generateToken} = require('../jwt');
+const Candidate = require('../models/candidate');
 
 
 const checkAdminRole = async (userID) => {
@@ -17,6 +16,7 @@ const checkAdminRole = async (userID) => {
    }
 }
 
+// POST route to add a candidate
 router.post('/', jwtAuthMiddleware, async (req, res) =>{
     try{
         if(!(await checkAdminRole(req.user.id)))
@@ -65,7 +65,16 @@ router.put('/:candidateID', jwtAuthMiddleware, async (req, res)=>{
 
 router.delete('/:candidateID', jwtAuthMiddleware, async (req, res)=>{
     try{
+        if(!checkAdminRole(req.user.id))
+            return res.status(403).json({message: 'user does not have admin role'});
         
+        const candidateID = req.params.candidateID; // Extract the id from the URL parameter
+
+        const response = await Candidate.findByIdAndDelete(candidateID);
+
+        if (!response) {
+            return res.status(404).json({ error: 'Candidate not found' });
+        }
 
         console.log('candidate deleted');
         res.status(200).json(response);
@@ -74,3 +83,82 @@ router.delete('/:candidateID', jwtAuthMiddleware, async (req, res)=>{
         res.status(500).json({error: 'Internal Server Error'});
     }
 })
+
+// let's start voting
+router.get('/vote/:candidateID', jwtAuthMiddleware, async (req, res)=>{
+    // no admin can vote
+    // user can only vote once
+    
+    candidateID = req.params.candidateID;
+    userId = req.user.id;
+
+    try{
+        // Find the Candidate document with the specified candidateID
+        const candidate = await Candidate.findById(candidateID);
+        if(!candidate){
+            return res.status(404).json({ message: 'Candidate not found' });
+        }
+
+        const user = await User.findById(userId);
+        if(!user){
+            return res.status(404).json({ message: 'user not found' });
+        }
+        if(user.role == 'admin'){
+            return res.status(403).json({ message: 'admin is not allowed'});
+        }
+        if(user.isVoted){
+            return res.status(400).json({ message: 'You have already voted' });
+        }
+
+        // Update the Candidate document to record the vote
+        candidate.votes.push({user: userId})
+        candidate.voteCount++;
+        await candidate.save();
+
+        // update the user document
+        user.isVoted = true
+        await user.save();
+
+        return res.status(200).json({ message: 'Vote recorded successfully' });
+    }catch(err){
+        console.log(err);
+        return res.status(500).json({error: 'Internal Server Error'});
+    }
+});
+
+// vote count 
+router.get('/vote/count', async (req, res) => {
+    try{
+        // Find all candidates and sort them by voteCount in descending order
+        const candidate = await Candidate.find().sort({voteCount: 'desc'});
+
+        // Map the candidates to only return their name and voteCount
+        const voteRecord = candidate.map((data)=>{
+            return {
+                party: data.party,
+                count: data.voteCount
+            }
+        });
+
+        return res.status(200).json(voteRecord);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+});
+
+// Get List of all candidates with only name and party fields
+router.get('/', async (req, res) => {
+    try {
+        // Find all candidates and select only the name and party fields, excluding _id
+        const candidates = await Candidate.find({}, 'name party -_id');
+
+        // Return the list of candidates
+        res.status(200).json(candidates);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+module.exports = router;

50e3eab4b88708386d1e8401306174bd81c4e416
somraj wadkar <somrajwadkar2@gmail.com>
2026-06-04 11:50:40 +0530

    the latest DB changes

--- DB.js
+++ DB.js
@@ -10,3 +10,15 @@ mongoose.connect(mongoURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
+
+const db = mongoose.connection;
+
+// Define event listeners for database connection
+
+db.on('connected', () => {
+    console.log('Connected to MongoDB server');
+});
+
+db.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+});

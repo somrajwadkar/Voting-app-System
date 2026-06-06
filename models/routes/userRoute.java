71c251da5da167c199bd56a97a46ab3db45c740c
somraj wadkar <somrajwadkar2@gmail.com>
2026-06-06 11:22:37 +0530

    UserRoute java

--- models/routes/userRoute.java
+++ models/routes/userRoute.java
@@ -45,3 +45,35 @@ router.post('/signup', async (req, res) =>{
         res.status(500).json({error: 'Internal Server Error'});
     }
 })
+
+router.post('/login', async(req, res) => {
+    try{
+        // Extract aadharCardNumber and password from request body
+        const {aadharCardNumber, password} = req.body;
+
+        // Check if aadharCardNumber or password is missing
+        if (!aadharCardNumber || !password) {
+            return res.status(400).json({ error: 'Aadhar Card Number and password are required' });
+        }
+
+        // Find the user by aadharCardNumber
+        const user = await User.findOne({aadharCardNumber: aadharCardNumber});
+
+        // If user does not exist or password does not match, return error
+        if( !user || !(await user.comparePassword(password))){
+            return res.status(401).json({error: 'Invalid Aadhar Card Number or Password'});
+        }
+
+        // generate Token 
+        const payload = {
+            id: user.id,
+        }
+        const token = generateToken(payload);
+
+        // resturn token as response
+        res.json({token})
+    }catch(err){
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});

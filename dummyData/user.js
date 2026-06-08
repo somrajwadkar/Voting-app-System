a8fc29e3b2d09d19bd623384604cf4b785b3c63e
somraj wadkar <somrajwadkar2@gmail.com>
2026-06-08 11:46:15 +0530

    adding the new user in thee list

---
+++ dummyData/user.js
@@ -0,0 +1,12 @@
+[
+    {
+        "name": "Ravi Kumar",
+        "age": 30,
+        "mobile": "9876543210",
+        "email": "ravi@example.com",
+        "address": "123, ABC Street, XYZ City",
+        "aadharCardNumber": 123456789012,
+        "password": "password123"
+    },
+  ]
+    

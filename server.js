d0ccea258a72c6367e3f86b4152e197114731454
somraj wadkar <somrajwadkar2@gmail.com>
2026-06-11 15:30:03 +0530

    the latest comit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-require('./DB');
+const db = require('./db');
 require('dotenv').config();
 
 const bodyParser = require('body-parser'); 
@@ -8,31 +8,14 @@ app.use(bodyParser.json()); // req.body
 const PORT = process.env.PORT || 3000;
 
 // Import the router files
-const userRoutes = require('./models/routes/userRoute');
-const candidateRoutes = require('./models/routes/candidateRoute');
+const userRoutes = require('./routes/userRoutes');
+const candidateRoutes = require('./routes/candidateRoutes');
 
 // Use the routers
 app.use('/user', userRoutes);
 app.use('/candidate', candidateRoutes);
 
+
 app.listen(PORT, ()=>{
     console.log('listening on port 3000');
 })
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-
-app.get('/api/data', (req, res) => {
-    // Simulate fetching data from a database or another source
-    const data = {
-        message: 'Hello from the API!',
-        timestamp: new Date()
-    };
-    res.json(data);
-});
- //git add .
-// git commit -m "Describe your changes"
-// git push origin main
-
